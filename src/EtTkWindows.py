f815cc6631563e44fb484555274fc8d323f70d72
Arthur Johnson Jr <arthurj@fullsail.com>
2016-01-06 15:25:46 -0500

    Added Table, a matrix of (editable) strings

--- src/EtTkWindows.py
+++ src/EtTkWindows.py
@@ -20,6 +20,7 @@ This file is part of the EdTech library project at Full Sail University.
 import ttk
 
 from Tkinter import *
+from itertools import repeat
 
 # http://stackoverflow.com/questions/3352918/how-to-center-a-window-on-the-screen-in-tkinter
 def TkCenter(win):
@@ -75,3 +76,69 @@ class FrameWithScrollbar(object):
         vsb.pack(side = "right", fill = "y")
         self.canvas.pack(side = "left", fill = "both", expand = True)
         self.frame.bind("<Configure>", lambda event, canvas = self.canvas: self.canvas.configure(scrollregion = self.canvas.bbox("all")))
+
+class Table(object):
+    """A matrix of (editable) strings."""
+    def __init__(self, root, width, height, col, row, columnspan, headers = None):
+        self.headers = headers
+        self.rows = row
+
+        self.stringVars = []
+        self.entries = []
+
+        canvas = Canvas(root, relief = GROOVE, borderwidth = 2, width = width, height = height)
+        self.frame = ttk.Frame(canvas)
+        self.frame.bind("<Configure>", lambda event, canvas = canvas: canvas.configure(scrollregion = canvas.bbox("all")))
+        if self.headers != None: self.Add([self.headers], True)
+
+        canvas.create_window((0, 0), window = self.frame, anchor = "nw")
+        canvas.grid(column = col, row = row, columnspan = columnspan)
+        HSB = ttk.Scrollbar(root, orient = "horizontal", command = canvas.xview)
+        VSB = ttk.Scrollbar(root, orient = "vertical", command = canvas.yview)
+        canvas.configure(xscrollcommand = HSB.set, yscrollcommand = VSB.set)
+        VSB.grid(column = col + columnspan, row = row, sticky = "ns")
+        row += 1
+        HSB.grid(column = col, row = row, columnspan = columnspan, sticky = "ew")
+        row += 1
+
+        self.rows = row - self.rows
+
+    def Reset(self):
+        num = len(self.stringVars)
+        if self.headers != None: num -= 1
+        for i in range(num):
+            self.stringVars.pop()
+            for e in self.entries.pop(): e.destroy()
+
+    def Add(self, rows, readonly = False):
+        row = len(self.stringVars)
+        for r in rows:
+            stringVars = []
+            entries = []
+            col = 0
+            for c in r:
+                sv = StringVar(value = c)
+                stringVars.append(sv)
+                e = ttk.Entry(self.frame, textvariable = sv, font = ("Consolas"))
+                if readonly: e.config(state = "readonly")
+                e.grid(row = row, column = col)
+                entries.append(e)
+                col += 1
+            row += 1
+            self.stringVars.append(stringVars)
+            self.entries.append(entries)
+        self.ResizeColumns()
+
+    def Rows(self): return self.rows
+
+    def ResizeColumns(self):
+        if len(self.stringVars) == 0 or len(self.stringVars[0]) == 0: return
+
+        widths = list(repeat(0, len(self.stringVars[0])))
+        for row in self.stringVars:
+            for col in range(len(row)):
+                widths[col] = max(widths[col], len(row[col].get()))
+
+        for row in self.entries:
+            for i in range(len(row)):
+                row[i].config(width = widths[i])
