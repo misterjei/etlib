3f970fb4b3ea9e3409faecbfe2a330c9f39678be
Jeremiah Blanchard <jeremiahblanchard@gmail.com>
2019-06-12 15:38:57 -0400

    Reformatted to create proper library package (now called 'edtech')

---
+++ edtech/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='edtech',
+    version='0.9',
+    packages=[''],
+    package_dir={'edtech',},
+    url='https://github.com/misterjei/etlib',
+    license='GPL 3',
+    author='Jeremiah Blanchard',
+    author_email='',
+    description=''
+)
